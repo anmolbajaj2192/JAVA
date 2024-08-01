499c991b3757bf14f0c69043e548a3a1dea8f04e
ANMOL BAJAJ <banmol241@gmail.com>
2024-08-01 21:51:10 +0530

    HasPath file

---
+++ 37-42_Graphs/Q1_HasPath.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+
+public class Q1_HasPath {
+
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+        
+        for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 0));
+
+        graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 0));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean vis[]){
+        System.out.println(curr+" ");
+        vis[curr] = true;
+
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+
+            if(!vis[e.dest]){
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+    public static boolean hasPath(ArrayList<Edge> graph[], int src, int dest, boolean vis[]){
+
+        if(src == dest){
+            return true;
+        }
+
+        vis[src] = true;
+
+        for(int i=0; i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            //e.dest = neighbour
+            if(!vis[e.dest] && hasPath(graph, e.dest, dest, vis)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String args[]) { //O(V+E)
+        /*
+
+            1 ---- 3
+            /      | \
+            0      |  5 -- 6
+            \      | /
+            2 ---- 4
+
+        */
+    
+            int V = 7;
+            ArrayList<Edge> graph[] = new ArrayList[V];
+            createGraph(graph);
+            System.out.println(hasPath(graph, 0, 5, new boolean[V]));
+
+    }
+    
+}
